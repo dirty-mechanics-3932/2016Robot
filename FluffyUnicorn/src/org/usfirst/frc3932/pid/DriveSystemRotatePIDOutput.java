39d5b28c3e7b8458534f11cd8f5b78773872128a
mbshark <myles.first@gmail.com>
2016-03-07 20:58:25 -0500

    PID values close enough

--- FluffyUnicorn/src/org/usfirst/frc3932/pid/DriveSystemRotatePIDOutput.java
+++ FluffyUnicorn/src/org/usfirst/frc3932/pid/DriveSystemRotatePIDOutput.java
@@ -5,7 +5,6 @@ import org.usfirst.frc3932.Robot;
 import edu.wpi.first.wpilibj.PIDOutput;
 
 public class DriveSystemRotatePIDOutput implements PIDOutput {
-
 	@Override
 	public void pidWrite(double output) {
 		Robot.driveSystem.drivePercent(-output, output);
