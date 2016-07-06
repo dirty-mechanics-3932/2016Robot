087afd3541e2adf3991d1d604608a09eb4926300
Thomas Pakh <thomas.pakh@gmail.com>
2016-07-06 16:46:18 -0400

    Initial commit. Added spotless formatting.

---
+++ src/test/java/org/test/usfirst/frc/team3932/robot/ExampleTest.java
@@ -0,0 +1,15 @@
+
+package org.test.usfirst.frc.team3932.robot;
+
+import org.junit.Test;
+import org.usfirst.frc.team3932.robot.Robot;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void testExample() {
+        Robot classUnderTest = new Robot();
+        // assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
