0d8541cb7f2c4a4954c07b96d4e8f71efce39dea
Sebastian Rossi <sebastian.rossi07@gmail.com>
2016-08-04 19:05:36 -0400

    Standardize ReturnToNormal class name and constructor, clean up mountHeight and mountAngle in Vision

--- FluffyUnicorn/src/org/usfirst/frc3932/commands/Vision.java
+++ FluffyUnicorn/src/org/usfirst/frc3932/commands/Vision.java
@@ -2,24 +2,26 @@ package org.usfirst.frc3932.commands;
 
 import org.usfirst.frc3932.Robot;
 import java.lang.Math;
+import java.util.HashMap;
+
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Vision {
 	private NetworkTable table = null;
-
+	
+	public static double angle;
+	public static double area;
+	public static double centerX;
+	public static int length;
+	public static double solidity;
 	private static final double H_RES = 640;
 	private static final double Y_RES = 480;
 	private static final double FIELD_OF_VIEW = 74;
-	private static double centerX;
-	private static double solidity;
-	private static double area;
-	private static double angle;
-	private static int length;
-	private static final HashMap<Integer, Double> mountAngle = new Hashmap<Integer, Double>(); // mount info should be in seperate class, this is temp
-	private static final HashMap<Integer, Double> mountHeight = new Hashmap<Integer, Double>();
+	private static final HashMap<Integer, Double> mountAngle = new HashMap<Integer, Double>(); // mount info should be in seperate class, this is temp
+	private static final HashMap<Integer, Double> mountHeight = new HashMap<Integer, Double>();
 	private static double xDist;
-	private static double totalYAngle
+	private static double totalYAngle;
 
 	public Vision() {
 		table = NetworkTable.getTable("GRIP/myContoursReport");
@@ -34,23 +36,30 @@ public class Vision {
 		SmartDashboard.putNumber("xDist", hasXDist ? xDist : 0);
 		SmartDashboard.putNumber("totalYAngle", hasXDist ? totalYAngle : 0);
 	}
+	public double getMountAngle(int robotType) {
+		mountAngle.put(0, (double) 0); //comp cam angle
+		mountAngle.put(1, (double) 0); //daughter cam angle
+		mountAngle.put(3, (double) 0);  //mini cam angle
+		return mountAngle.get(robotType);
+	}
+	public double getMountHeight(int robotType) {
+		mountHeight.put(0, (double) 0); //comp cam height
+		mountHeight.put(1, (double) 0); //daughter cam height
+		mountHeight.put(3, (double) 0);  //mini cam height
+		return mountHeight.get(robotType);
+	}
 
 	public double getDist() {
+		double[] defaultValue = new double[0];
 		table = NetworkTable.getTable("GRIP/myContoursReport");
-		mountAngle.put(0, 0); //comp cam angle
-		mountAngle.put(1, 0); //daughter cam angle
-		mountAngle.put(3, 0);  //mini cam angle
-		mountHeight.put(0, 0); //comp cam height
-		mountHeight.put(1, 0); //daughter cam height
-		mountHeight.put(3, 0);  //mini cam height
 		double[] solidityAr = table.getNumberArray("solidity", defaultValue);
 		double[] centerYAr = table.getNumberArray("centerY", defaultValue);
 		xDist = -1; //dist to target in inches
 		length = solidityAr.length;
-		objIndex = getObj(length, solidityAr);
+		int objIndex = getObj(length, solidityAr);
 		if (!(objIndex == -1)) {
-			totalYAngle = mountAngle.get(Robot.robotType) + (centerYAr[objIndex] - (Y_RES / 2) * (FIELD_OF_VIEW / Y_RES));
-			xDist = (91 - mountHeight.get(Robot.robotType)) / Math.tan(Math.toRadians(totalYAngle)); //target is 91 in tall
+			totalYAngle = getMountAngle(Robot.robotType) + (centerYAr[objIndex] - (Y_RES / 2) * (FIELD_OF_VIEW / Y_RES));
+			xDist = (91 - getMountHeight(Robot.robotType)) / Math.tan(Math.toRadians(totalYAngle)); //target is 91 in tall
 		}
 		return xDist;
 	}
@@ -75,12 +84,12 @@ public class Vision {
 		double[] areaAr = table.getNumberArray("area", defaultValue);
 		boolean found = false;
 		length = solidityAr.length;
-		objIndex = getObj(length, solidityAr);
+		int objIndex = getObj(length, solidityAr);
 		if (!(objIndex == -1)) {
-			angle = (centerXAr[index] - (H_RES / 2)) / (X_RES / FIELD_OF_VIEW); // 1/10.4 degrees/pixels height of cam = 1ft, height of target = 7 ft 1, c x = 10 in from either side,										// 6 in from bot of u, cam view angle = 67º area of u online
-			area = areaAr[index];
-			centerX = centerXAr[index];
-			solidity = solidityAr[index];
+			angle = (centerXAr[objIndex] - (H_RES / 2)) / (H_RES / FIELD_OF_VIEW); // 1/10.4 degrees/pixels height of cam = 1ft, height of target = 7 ft 1, c x = 10 in from either side,										
+			area = areaAr[objIndex];
+			centerX = centerXAr[objIndex];
+			solidity = solidityAr[objIndex];
 			found = true;
 		}
 		return found;
